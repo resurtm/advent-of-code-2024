f08e3867fa8e64523155fd752bbbeb7a8115bfef
resurtm <resurtm@gmail.com>
2024-12-05 18:42:02 +0100

    Day 05, parse input + preps

---
+++ rust/src/day05.rs
@@ -0,0 +1,82 @@
+use std::fs;
+
+pub fn solve() {
+    PrintQueue::new(String::from("test0")).solve();
+}
+
+impl PrintQueue {
+    fn new(test_name: String) -> PrintQueue {
+        let (orders, pages) = Self::read_input(&test_name);
+        PrintQueue {
+            orders,
+            pages,
+            part1: 0,
+            part2: 0,
+            test_name,
+        }
+    }
+
+    fn solve(&self) -> (i32, i32) {
+        println!("Test Name: {}", self.test_name);
+        println!("Orders: {:?}", self.orders);
+        println!("Pages: {:?}", self.pages);
+        println!("Day 01, Part 1: {}", self.part1);
+        println!("Day 01, Part 2: {}", self.part2);
+        (self.part1, self.part2)
+    }
+
+    fn read_input(test_name: &str) -> (Vec<(i32, i32)>, Vec<Vec<i32>>) {
+        let raw =
+            fs::read_to_string(format!("../data/day05/{}.txt", test_name)).expect("input file");
+        let parts = raw.split("\n\n").collect::<Vec<&str>>();
+        let orders = parts
+            .get(0)
+            .expect("orders")
+            .split("\n")
+            .filter(|x| !x.is_empty())
+            .map(|x| x.split("|").collect::<Vec<&str>>())
+            .map(|x| {
+                (
+                    x.get(0)
+                        .expect("left order")
+                        .parse::<i32>()
+                        .expect("left i32"),
+                    x.get(1)
+                        .expect("right order")
+                        .parse::<i32>()
+                        .expect("right i32"),
+                )
+            })
+            .collect::<Vec<(i32, i32)>>();
+        let pages = parts
+            .get(1)
+            .expect("pages")
+            .split("\n")
+            .filter(|x| !x.is_empty())
+            .map(|x| {
+                x.split(",")
+                    .map(|x| x.parse::<i32>().expect("page i32"))
+                    .collect()
+            })
+            .collect::<Vec<Vec<i32>>>();
+        (orders, pages)
+    }
+}
+
+struct PrintQueue {
+    orders: Vec<(i32, i32)>,
+    pages: Vec<Vec<i32>>,
+    part1: i32,
+    part2: i32,
+    test_name: String,
+}
+
+#[cfg(test)]
+mod tests {
+    use super::PrintQueue;
+
+    #[test]
+    fn test_part1() {
+        assert_eq!(PrintQueue::new(String::from("test0")).solve().0, 18);
+    }
+}
--- rust/src/main.rs
+++ rust/src/main.rs
@@ -2,5 +2,6 @@ fn main() {
     // aoc24::day01::solve();
     // aoc24::day02::solve();
     // aoc24::day03::solve();
-    aoc24::day04::solve();
+    // aoc24::day04::solve();
+    aoc24::day05::solve();
 }
