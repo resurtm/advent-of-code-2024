561194dd8fb777b97b173019c17be1d9781a6b77
resurtm <resurtm@gmail.com>
2024-12-06 07:21:39 +0100

    Day 06, find start pos

--- rust/src/day06.rs
+++ rust/src/day06.rs
@@ -6,23 +6,44 @@ pub fn solve() {
 
 impl GuardGallivant {
     fn new(test_name: String) -> GuardGallivant {
+        let tiles = Self::read_input(&test_name);
+        let w = tiles.len() as i32;
+        let h = tiles[0].len() as i32;
         GuardGallivant {
-            tiles: Self::read_input(&test_name),
+            tiles,
+            w,
+            h,
+            pos: (0, 0),
             part1: 0,
             part2: 0,
             test_name,
         }
     }
 
-    fn solve_internal(&mut self) {}
+    fn solve_internal(&mut self) {
+        self.find_start();
+    }
 
     fn solve(&mut self) -> (i32, i32) {
         self.solve_internal();
         println!("Test Name: {}", self.test_name);
         println!("Tiles: {:?}", self.tiles);
+        println!("Pos: {:?}", self.pos);
         println!("Day 06, Part 1: {}", self.part1);
         println!("Day 06, Part 2: {}", self.part2);
         (self.part1, self.part2)
+    }
+
+    fn find_start(&mut self) {
+        for x in 0..self.w {
+            for y in 0..self.h {
+                if self.tiles[x as usize][y as usize] == '^' {
+                    self.pos = (x, y);
+                    return;
+                }
+            }
+        }
+        panic!("invalid input")
     }
 
     fn read_input(test_name: &str) -> Vec<Vec<char>> {
@@ -41,6 +62,9 @@ impl GuardGallivant {
 
 struct GuardGallivant {
     tiles: Vec<Vec<char>>,
+    w: i32,
+    h: i32,
+    pos: (i32, i32),
     part1: i32,
     part2: i32,
     test_name: String,
