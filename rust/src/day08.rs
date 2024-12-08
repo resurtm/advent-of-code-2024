89de6e156a2c209ed03443d0a89f3d73b748371d
resurtm <resurtm@gmail.com>
2024-12-08 08:50:43 +0100

    Day 08, part 1, Rust

--- rust/src/day08.rs
+++ rust/src/day08.rs
@@ -2,8 +2,8 @@ use std::{collections::HashMap, fs};
 
 pub fn solve() {
     ResonantCollinearity::new(String::from("test0")).solve();
-    // ResonantCollinearity::new(String::from("gh")).solve();
-    // ResonantCollinearity::new(String::from("google")).solve();
+    ResonantCollinearity::new(String::from("gh")).solve();
+    ResonantCollinearity::new(String::from("google")).solve();
 }
 
 impl ResonantCollinearity {
@@ -22,12 +22,48 @@ impl ResonantCollinearity {
     }
 
     fn solve_internal(&mut self) {
-        println!("{:?}", self.ant);
+        for (ch, pos) in self.ant.iter() {
+            for m in 0..pos.len() {
+                for n in m + 1..pos.len() {
+                    let delta = ((pos[m].0 - pos[n].0), (pos[m].1 - pos[n].1));
+                    let its = vec![
+                        (pos[m].0 - delta.0, pos[m].1 - delta.1),
+                        (pos[m].0 + delta.0, pos[m].1 + delta.1),
+                        (pos[n].0 - delta.0, pos[n].1 - delta.1),
+                        (pos[n].0 + delta.0, pos[n].1 + delta.1),
+                    ];
+                    for (x, y) in its.iter() {
+                        if *x < 0 || *y < 0 || *x >= self.w || *y >= self.h {
+                            continue;
+                        }
+                        if self.map[*x as usize][*y as usize] == *ch {
+                            continue;
+                        }
+                        self.map[*x as usize][*y as usize] = '#';
+                    }
+                }
+            }
+            // self.print_map();
+        }
     }
 
-    fn solve(&mut self) -> (i128, i128) {
+    fn find_fills(&self) -> i32 {
+        let mut ret = 0;
+        for i in 0..self.w {
+            for j in 0..self.h {
+                if self.map[i as usize][j as usize] == '#' {
+                    ret += 1;
+                }
+            }
+        }
+        ret
+    }
+
+    fn solve(&mut self) -> (i32, i32) {
         self.find_ants();
         self.solve_internal();
+        self.part1 = self.find_fills();
+        // self.print_map();
         println!("Test Name: {}", self.test_name);
         println!("Day 08, Part 1: {}", self.part1);
         println!("Day 08, Part 2: {}", self.part2);
@@ -48,6 +84,17 @@ impl ResonantCollinearity {
                 }
             }
         }
+    }
+
+    fn print_map(&self) {
+        println!("{}", "-".repeat(self.w as usize));
+        for i in 0..self.w {
+            for j in 0..self.h {
+                print!("{}", self.map[i as usize][j as usize]);
+            }
+            println!("");
+        }
+        println!("{}", "-".repeat(self.w as usize));
     }
 
     fn read_input(test_name: &str) -> Vec<Vec<char>> {
@@ -71,8 +118,8 @@ struct ResonantCollinearity {
     map: Vec<Vec<char>>,
     ant: HashMap<char, Vec<(i32, i32)>>,
 
-    part1: i128,
-    part2: i128,
+    part1: i32,
+    part2: i32,
     test_name: String,
 }
 
