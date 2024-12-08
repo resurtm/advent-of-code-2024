bfff172c59445170a4fc113c6dde373aa7a71f25
resurtm <resurtm@gmail.com>
2024-12-08 08:31:54 +0100

    Day 08, find antennas

--- rust/src/day08.rs
+++ rust/src/day08.rs
@@ -1,4 +1,4 @@
-use std::fs;
+use std::{collections::HashMap, fs};
 
 pub fn solve() {
     ResonantCollinearity::new(String::from("test0")).solve();
@@ -8,19 +8,46 @@ pub fn solve() {
 
 impl ResonantCollinearity {
     fn new(test_name: String) -> ResonantCollinearity {
+        let map = Self::read_input(&test_name);
         ResonantCollinearity {
-            map: Self::read_input(&test_name),
+            w: map.len() as i32,
+            h: map[0].len() as i32,
+            map,
+            ant: HashMap::new(),
+
             part1: 0,
             part2: 0,
             test_name,
         }
     }
 
+    fn solve_internal(&mut self) {
+        println!("{:?}", self.ant);
+    }
+
     fn solve(&mut self) -> (i128, i128) {
+        self.find_ants();
+        self.solve_internal();
         println!("Test Name: {}", self.test_name);
         println!("Day 08, Part 1: {}", self.part1);
         println!("Day 08, Part 2: {}", self.part2);
         (self.part1, self.part2)
+    }
+
+    fn find_ants(&mut self) {
+        for i in 0..self.w {
+            for j in 0..self.h {
+                let ch = self.map[i as usize][j as usize];
+                if ch == '.' {
+                    continue;
+                }
+                if self.ant.contains_key(&ch) {
+                    self.ant.get_mut(&ch).unwrap().push((i, j));
+                } else {
+                    self.ant.insert(ch, vec![(i, j)]);
+                }
+            }
+        }
     }
 
     fn read_input(test_name: &str) -> Vec<Vec<char>> {
@@ -39,7 +66,11 @@ impl ResonantCollinearity {
 }
 
 struct ResonantCollinearity {
+    w: i32,
+    h: i32,
     map: Vec<Vec<char>>,
+    ant: HashMap<char, Vec<(i32, i32)>>,
+
     part1: i128,
     part2: i128,
     test_name: String,
