a85efe15aee6acef7a58aa2b3bed58564d0e2183
resurtm <resurtm@gmail.com>
2024-12-14 10:45:16 +0100

    Day 14, part 2, Rust

--- rust/src/day14.rs
+++ rust/src/day14.rs
@@ -1,35 +1,92 @@
 use regex::Regex;
-use std::fs;
+use std::{
+    collections::{HashSet, VecDeque},
+    fs,
+};
 
 pub fn solve() {
-    RestroomRedoubt::new(11, 7, String::from("test0")).solve();
+    // RestroomRedoubt::new(11, 7, String::from("test0")).solve();
     RestroomRedoubt::new(101, 103, String::from("gh")).solve();
     RestroomRedoubt::new(101, 103, String::from("google")).solve();
+    // RestroomRedoubt::new(15, 15, String::from("test1")).solve();
 }
 
 impl RestroomRedoubt {
-    fn simulate(&mut self) {
-        for _ in 0..100 {
+    fn simulate(&mut self, seconds: i32, do_check: bool) -> i32 {
+        for second in 0..seconds {
             for robot in self.robots.iter_mut() {
                 robot.p.0 += robot.v.0;
                 robot.p.1 += robot.v.1;
                 Self::limit(robot, self.w, self.h);
             }
+            if do_check {
+                self.build_map();
+                if self.check_tree() {
+                    self.print();
+                    return second + 101;
+                }
+            }
         }
-        // self.print();
+        0
+    }
+
+    fn check_tree(&self) -> bool {
+        // let mut areas = 0;
+        let mut found = false;
+        let mut viz: HashSet<(i32, i32)> = HashSet::new();
+        for st in self.robots.iter() {
+            if viz.contains(&(st.p.0, st.p.1)) {
+                continue;
+            }
+            let mut area: HashSet<(i32, i32)> = HashSet::new();
+            let mut deq: VecDeque<(i32, i32)> = VecDeque::new();
+            deq.push_back((st.p.0, st.p.1));
+            while !deq.is_empty() {
+                let (m, n) = deq.pop_front().unwrap();
+                vec![
+                    (m - 1, n - 1),
+                    (m - 1, n),
+                    (m - 1, n + 1),
+                    (m + 1, n - 1),
+                    (m + 1, n),
+                    (m + 1, n + 1),
+                    (m, n - 1),
+                    (m, n + 1),
+                ]
+                .iter()
+                .for_each(|(u, v)| {
+                    let tmp = (*u, *v);
+                    if tmp.0 >= 0
+                        && tmp.1 >= 0
+                        && tmp.0 < self.w
+                        && tmp.1 < self.h
+                        && self.map[tmp.0 as usize][tmp.1 as usize]
+                        && !viz.contains(&tmp)
+                        && !area.contains(&tmp)
+                    {
+                        viz.insert(tmp);
+                        area.insert(tmp);
+                        deq.push_back(tmp);
+                    }
+                });
+            }
+            // areas += 1;
+            if area.len() >= self.robots.len() / 3 {
+                found = true
+            }
+        }
+        found
     }
 
     fn limit(robot: &mut Robot, w: i32, h: i32) {
         if robot.p.0 < 0 {
-            robot.p.0 = w + robot.p.0;
-        }
-        if robot.p.1 < 0 {
-            robot.p.1 = h + robot.p.1;
-        }
-        if robot.p.0 >= w {
+            robot.p.0 = robot.p.0 + w;
+        } else if robot.p.0 >= w {
             robot.p.0 = robot.p.0 - w;
         }
-        if robot.p.1 >= h {
+        if robot.p.1 < 0 {
+            robot.p.1 = robot.p.1 + h;
+        } else if robot.p.1 >= h {
             robot.p.1 = robot.p.1 - h;
         }
     }
@@ -79,14 +136,26 @@ impl RestroomRedoubt {
     }
 
     fn solve(&mut self) -> (i32, i32) {
-        self.simulate();
+        self.simulate(100, false);
         self.part1 = self.calculate_safety();
+        self.part2 = self.simulate(10_000, true);
 
         println!("Test Name: {}", self.test_name);
         println!("Day 14, Part 1: {}", self.part1);
         println!("Day 14, Part 2: {}", self.part2);
 
         (self.part1, self.part2)
+    }
+
+    fn build_map(&mut self) {
+        self.map.clear();
+        self.map.resize(self.w as usize, vec![]);
+        for it in self.map.iter_mut() {
+            it.resize(self.h as usize, false);
+        }
+        for robot in self.robots.iter() {
+            self.map[robot.p.0 as usize][robot.p.1 as usize] = true;
+        }
     }
 
     #[allow(dead_code)]
@@ -132,10 +201,9 @@ impl RestroomRedoubt {
         let robots = Self::read_input(&test_name);
         RestroomRedoubt {
             robots,
-
+            map: Vec::new(),
             w,
             h,
-
             part1: 0,
             part2: 0,
             test_name,
@@ -154,10 +222,9 @@ impl Robot {
 
 struct RestroomRedoubt {
     robots: Vec<Robot>,
-
+    map: Vec<Vec<bool>>,
     w: i32,
     h: i32,
-
     part1: i32,
     part2: i32,
     test_name: String,
@@ -185,7 +252,7 @@ mod tests {
     fn test_part2() {
         assert_eq!(
             RestroomRedoubt::new(11, 7, String::from("test0")).solve().1,
-            0
+            104
         );
     }
 }
