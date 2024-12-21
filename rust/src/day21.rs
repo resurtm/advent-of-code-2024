376cd54242ae35e7bb7c476f797c03a34e64c7f7
resurtm <resurtm@gmail.com>
2024-12-21 13:29:42 +0100

    Day 21, part 1, BFS with routes, WIP

--- rust/src/day21.rs
+++ rust/src/day21.rs
@@ -1,4 +1,7 @@
-use std::{collections::HashMap, fs};
+use std::{
+    collections::{HashMap, VecDeque},
+    fs,
+};
 
 pub fn solve() {
     KeypadConundrum::new(String::from("test0")).solve();
@@ -7,68 +10,99 @@ pub fn solve() {
 }
 
 impl KeypadConundrum {
-    fn simulate(actions: &str, keypad: &HashMap<char, (i32, i32)>) -> String {
-        let mut pos = keypad[&'A'];
-        let mut res = vec![];
+    fn bfs(&self, src: char, dst: char) -> Vec<Vec<(i32, i32)>> {
+        let mut routes: Vec<Vec<(i32, i32)>> = vec![];
 
-        for action in actions.chars() {
-            let dst = keypad[&action];
-            let diff = (dst.0 - pos.0, dst.1 - pos.1);
+        let mut queue: VecDeque<Vec<(i32, i32)>> = VecDeque::new();
+        queue.push_back(vec![self.keypad0[&src]]);
 
-            match diff.1.cmp(&0) {
-                std::cmp::Ordering::Less => {
-                    (0..-diff.1).for_each(|_| res.push('^'));
-                }
-                std::cmp::Ordering::Equal => {}
-                std::cmp::Ordering::Greater => {
-                    (0..diff.1).for_each(|_| res.push('v'));
+        while !queue.is_empty() {
+            if let Some(route) = queue.pop_front() {
+                if let Some(&last) = route.last() {
+                    if self.keypad0[&dst] == last {
+                        routes.push(route);
+                        continue;
+                    }
+                    [
+                        (last.0 - 1, last.1),
+                        (last.0 + 1, last.1),
+                        (last.0, last.1 - 1),
+                        (last.0, last.1 + 1),
+                    ]
+                    .iter()
+                    .filter(|(x, y)| *x >= 0 && *y >= 0 && *x <= 2 && *y <= 3)
+                    .filter(|x| !route.contains(x))
+                    .for_each(|x| {
+                        let mut new_route = route.clone();
+                        new_route.push(*x);
+                        queue.push_back(new_route);
+                    });
                 }
             }
-            match diff.0.cmp(&0) {
-                std::cmp::Ordering::Less => {
-                    (0..-diff.0).for_each(|_| res.push('<'));
-                }
-                std::cmp::Ordering::Equal => {}
-                std::cmp::Ordering::Greater => {
-                    (0..diff.0).for_each(|_| res.push('>'));
-                }
-            }
-            res.push('A');
-
-            pos = dst;
         }
 
-        let mut res_str = String::new();
-        res.iter().for_each(|ch| res_str.push(*ch));
-        res_str
-    }
-
-    fn code_nums(code: &str) -> i32 {
-        code.chars()
-            .filter(|&x| x >= '0' && x <= '9')
-            .collect::<String>()
-            .parse::<i32>()
-            .unwrap_or(0)
-    }
-
-    fn solve_internal(&self) -> i32 {
-        let mut res = 0;
-        for input in self.input.iter() {
-            let pushes0 = Self::simulate(input, &self.keypad0);
-            let pushes1 = Self::simulate(&pushes0, &self.keypad1);
-            let pushes2 = Self::simulate(&pushes1, &self.keypad1);
-
-            let a = pushes2.len() as i32;
-            let b = Self::code_nums(input);
-
-            println!("{} {} {}", a, b, a * b);
-            res += a * b;
+        let min_route = routes.iter().map(|x| x.len()).min().unwrap_or(0);
+        let mut res = vec![];
+        for route in routes.iter() {
+            if route.len() == min_route {
+                res.push(route.clone());
+            }
         }
         res
     }
 
+    fn solve_internal(&self, items: &str) {
+        let mut routes: Vec<Vec<(i32, i32)>> = vec![];
+        let mut curr_item = 'A';
+
+        for item in items.chars() {
+            let calc_routes = self.bfs(curr_item, item);
+
+            let mut next_routes = vec![];
+            for calc_route in calc_routes.iter() {
+                if routes.is_empty() {
+                    next_routes.push(calc_route.clone());
+                } else {
+                    for route in routes.iter() {
+                        let mut next_route = route.clone();
+                        next_route.extend(calc_route);
+                        next_routes.push(next_route);
+                    }
+                }
+            }
+            routes = next_routes;
+
+            curr_item = item;
+        }
+
+        for route in routes.iter() {
+            println!("{} - {}", route.len(), Self::route_to_chars(route));
+        }
+    }
+
+    fn route_to_chars(route: &[(i32, i32)]) -> String {
+        let mut res = String::new();
+        for idx in 0..route.len() - 1 {
+            let curr = route[idx];
+            let next = route[idx + 1];
+            if curr.0 - next.0 > 0 {
+                res.push('<');
+            } else if curr.0 - next.0 < 0 {
+                res.push('>');
+            } else if curr.1 - next.1 > 0 {
+                res.push('^');
+            } else if curr.1 - next.1 < 0 {
+                res.push('v');
+            } else {
+                res.push('A');
+            }
+        }
+        res.push('A');
+        res
+    }
+
     fn solve(&mut self) -> (i32, i32) {
-        self.part1 = self.solve_internal();
+        self.solve_internal("029A");
 
         println!("Test Name: {}", self.test_name);
         println!("Day 21, Part 1: {}", self.part1);
@@ -98,10 +132,12 @@ impl KeypadConundrum {
             ('1', (0, 2)),
             ('2', (1, 2)),
             ('3', (2, 2)),
+            (' ', (0, 3)),
             ('0', (1, 3)),
             ('A', (2, 3)),
         ]);
         let keypad1 = HashMap::from([
+            (' ', (0, 0)),
             ('^', (1, 0)),
             ('A', (2, 0)),
             ('<', (0, 1)),
